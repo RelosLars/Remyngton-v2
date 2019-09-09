d2a7546144793066350b40b1a10d5870638f736a
larssoler <larssoler@hotmail.com>
2019-09-10 01:13:38 +0200

    Tournament database created

---
+++ Database Create Query.sql
@@ -0,0 +1,25 @@
+﻿create table tbl_Tournaments
+(
+	TournamentName nvarchar(128) primary key,
+	StartDate datetime,
+	TeamlistLink nvarchar(128)
+)
+
+create table tbl_ScoringSystem
+(
+	ScoringName nvarchar(64) primary key
+)
+
+create table tbl_Stage
+(
+	TournamentName nvarchar(128) foreign key references tbl_Tournaments(TournamentName),
+	StageName nvarchar(64),
+	ScoringSystem nvarchar(64) foreign key references tbl_ScoringSystem(ScoringName)
+)
+
+
+create table tbl_TournamentParticipants
+(
+	TournamentName nvarchar(128) foreign key references tbl_Tournaments(TournamentName),
+	UserID nvarchar(16),
+)
--- Remyngton v2/Tournaments.aspx
+++ Remyngton v2/Tournaments.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="Remyngton_v2.Contact" %>
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Tournaments.aspx.cs" Inherits="Remyngton_v2.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %>.</h2>
